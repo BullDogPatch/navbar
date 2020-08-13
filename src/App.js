0c60cff2abd243975c3f45450978b651934b93df
Craig Clayton <55770352+BullDogPatch@users.noreply.github.com>
2020-08-13 12:19:26 +0100

    . added a comment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+// nav needs to transition out a little
 function App() {
   return (
     <div>
