924997d96dee9bf9bd20fc3b2e36cddda0cb5aba
Craig Clayton <55770352+BullDogPatch@users.noreply.github.com>
2020-08-16 13:29:16 +0100

    ... ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
